ec20a4fd2ffd557bfdccdd89794f7e797c1f7cee
ajay682000 <ajaykrishnan682000.ak@gmail.com>
2024-03-16 10:08:25 +0530

    Dynamic api key

--- tester.py
+++ tester.py
@@ -1,8 +1,14 @@
-import llms
 
-llm = llms.llm
 
-def write_test_function(code):
+from langchain_google_genai import ChatGoogleGenerativeAI
+
+
+
+def write_test_function(code, api_key):
+    llm = ChatGoogleGenerativeAI(
+    google_api_key=api_key,
+    model="gemini-pro"
+    )
     template = f"""
 You are a Software Developer. Your task is to write a unit test for the below Code unit the unittest.
 
--- ui.py
+++ ui.py
@@ -3,9 +3,22 @@ import tester
 
 st.title("Unit Tester")
 
+
+
+api_key = st.sidebar.text_input("Enter Your Google API key:")
+api_key_button = st.sidebar.button("Submit", key="api_button")
+if api_key_button:
+    st.session_state.google_api_key = api_key
+    st.sidebar.write("Success ")
+
+
+
 input_code = st.text_area("Enter the Code", height=20*15)
-submit  = st.button("Submit")
+submit  = st.button("Submit", key="submit_button")
 if submit:
-    # st.write(text_box)
-    result =  tester.write_test_function(input_code)
+    result =  tester.write_test_function(input_code, st.session_state.google_api_key)
     st.write(result)
+clear_button = st.button("Clear")
+main_content = st.empty()
+if clear_button:
+    main_content.empty()
